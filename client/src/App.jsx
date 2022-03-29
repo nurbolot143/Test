8909fdd1386798c76c255f3393ca30e87e1ab96c
Nurbolot Boobekov <nurbolot.boobekov.pls@gmail.com>
2022-03-29 14:40:08 +0300

    fixed buh

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,20 +2,11 @@ import "./App.scss";
 import "./components/Nav";
 import Nav from "./components/Nav";
 import { Routes, Route } from "react-router-dom";
-import Day from "./pages/Day";
-// import Tuesday from "./pages/Tuesday";
-// import Wednesday from "./pages/Wednesday";
-// import Thursday from "./pages/Thursday";
-// import Friday from "./pages/Friday";
-// import { useState } from "react";
-
-const days = [
-  { day: "monday", number: 0 },
-  { day: "tuesday", number: 0 },
-  { day: "wednesday", number: 0 },
-  { day: "thursday", number: 0 },
-  { day: "friday", number: 0 },
-];
+import Monday from "./pages/Monday";
+import Tuesday from "./pages/Tuesday";
+import Wednesday from "./pages/Wednesday";
+import Thursday from "./pages/Thursday";
+import Friday from "./pages/Friday";
 
 function App() {
   return (
@@ -25,13 +16,11 @@ function App() {
         <h1 className="main__title">Задачи</h1>
 
         <Routes>
-          {days.map((el, idx) => {
-            return <Route key={idx} path={el.day} element={<Day />} />;
-          })}
-          {/* <Route path="tuesday" element={<Tuesday />} />
+          <Route path="monday" element={<Monday />} />
+          <Route path="tuesday" element={<Tuesday />} />
           <Route path="wednesday" element={<Wednesday />} />
           <Route path="thursday" element={<Thursday />} />
-          <Route path="friday" element={<Friday />} /> */}
+          <Route path="friday" element={<Friday />} />
         </Routes>
       </div>
     </div>
